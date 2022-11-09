97831a0b2aac2c1a988dcae54d3ce5a9c2121d79
Marcio Pelegrini <marcio.pelegrini@gmail.com>
2022-11-09 12:44:17 -0500

    First commit

---
+++ FastFood_Mar/FastFood_Mar/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using FastFood_Mar.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FastFood_Mar.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FastFood_Mar/FastFood_Mar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FastFood_Mar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
