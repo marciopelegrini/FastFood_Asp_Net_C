602b1cf727e72839ab66bf4e0e463e88c20de438
Marcio Pelegrini <marcio.pelegrini@gmail.com>
2023-02-08 14:53:58 -0500

    Routes

--- FastFood_Mar/FastFood_Mar/Components/CarrinhoCompraResumo.cs
+++ FastFood_Mar/FastFood_Mar/Components/CarrinhoCompraResumo.cs
@@ -15,13 +15,7 @@ namespace FastFood_Mar.Components
 
         public IViewComponentResult Invoke()
         {
-            //var itens = _carrinhoCompra.GetCarrinhoCompraItens();
-
-            var itens = new List<CarrinhoCompraItem>()
-            { 
-                new CarrinhoCompraItem(),
-                new CarrinhoCompraItem()
-            };
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
 
             _carrinhoCompra.CarrinhoCompraItems = itens;
 
--- FastFood_Mar/FastFood_Mar/Controllers/LancheController.cs
+++ FastFood_Mar/FastFood_Mar/Controllers/LancheController.cs
@@ -16,39 +16,47 @@ namespace FastFood_Mar.Controllers
 
         public IActionResult List(string categoria)
         {
-            IEnumerable<Lanche> Lanches;
+            IEnumerable<Lanche> lanches;
             string categoriaAtual = string.Empty;
 
             if (string.IsNullOrEmpty(categoria))
             {
-                Lanches = _lancheRepository.Lanches.OrderBy(l => l.LancheId);
+                lanches = _lancheRepository.Lanches.OrderBy(l => l.LancheId);
                 categoriaAtual = "Todos os lanches";
             }
             else
             {
-                if (string.Equals("Normal", categoria, StringComparison.OrdinalIgnoreCase))
-                {
-                    Lanches = _lancheRepository.Lanches
-                        .Where(l => l.Categoria.CategoriaNome.Equals("Normal"))
-                        .OrderBy(l => l.Nome);
-                }
-                else
-                {
-                    Lanches = _lancheRepository.Lanches
-                        .Where(l => l.Categoria.CategoriaNome.Equals("Natural"))
-                        .OrderBy(l => l.Nome);
-                }
+                //if (string.Equals("Normal", categoria, StringComparison.OrdinalIgnoreCase))
+                //{
+                //    lanches = _lancheRepository.Lanches
+                //        .Where(l => l.Categoria.CategoriaNome.Equals("Normal"))
+                //        .OrderBy(l => l.Nome);
+                //}
+                //else
+                //{
+                //    lanches = _lancheRepository.Lanches
+                //       .Where(l => l.Categoria.CategoriaNome.Equals("Natural"))
+                //       .OrderBy(l => l.Nome);
+                //}
+                lanches = _lancheRepository.Lanches
+                          .Where(l => l.Categoria.CategoriaNome.Equals(categoria))
+                          .OrderBy(c => c.Nome);
+
                 categoriaAtual = categoria;
             }
 
             var lanchesListViewModel = new LancheListViewModel
             {
-                Lanches = Lanches,
+                Lanches = lanches,
                 CategoriaAtual = categoriaAtual
             };
 
-
             return View(lanchesListViewModel);
+        }
+        public IActionResult Details(int lancheId)
+        {
+            var lanche = _lancheRepository.Lanches.FirstOrDefault(l => l.LancheId == lancheId);
+            return View(lanche);
         }
     }
 }
