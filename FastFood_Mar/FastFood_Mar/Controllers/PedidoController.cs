2d13a1fbbaac7f997f5e2e3d98301ad088c41ebd
Marcio Pelegrini <marcio.pelegrini@gmail.com>
2023-02-08 21:57:08 -0500

    View Checkout

---
+++ FastFood_Mar/FastFood_Mar/Controllers/PedidoController.cs
@@ -0,0 +1,30 @@
+using FastFood_Mar.Models;
+using FastFood_Mar.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastFood_Mar.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra)
+        {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido)
+        {
+            return View();
+        }
+    }
+}
