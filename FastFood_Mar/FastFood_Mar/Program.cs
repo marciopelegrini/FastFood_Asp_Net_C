93f1355602c19ab4efeca808c5f3fd4d15cbc313
Marcio Pelegrini <marcio.pelegrini@gmail.com>
2023-02-14 16:47:45 -0500

    Ended projet

--- FastFood_Mar/FastFood_Mar/Program.cs
+++ FastFood_Mar/FastFood_Mar/Program.cs
@@ -1,17 +1,121 @@
-namespace FastFood_Mar;
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args)
-           .Build()
-           .Run();
-    }
+using FastFood_Mar.Areas.Admin.Servicos;
+using FastFood_Mar.Context;
+using FastFood_Mar.Models;
+using FastFood_Mar.Repositories.Interfaces;
+using FastFood_Mar.Repositories;
+using FastFood_Mar.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using ReflectionIT.Mvc.Paging;
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+var builder = WebApplication.CreateBuilder(args);
+
+// Conector para o SQLServer
+//var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+//builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
+
+//Conector para o MySql
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
+var connection = builder.Configuration.GetConnectionString("MySQLConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connection, serverVersion));
+
+//Conector para SQLite
+//var connection = builder.Configuration.GetConnectionString("SQLiteConnection");
+//builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connection));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(options => options.AccessDeniedPath = "/Home/AccessDenied");
+builder.Services.Configure<ConfigurationImagens>(builder.Configuration.GetSection("ConfigurationPastaImagens"));
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Default Password settings.
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 3;
+    options.Password.RequiredUniqueChars = 1;
+});
+
+builder.Services.AddTransient<ILancheRepository, LancheRepository>();
+builder.Services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+builder.Services.AddTransient<IPedidoRepository, PedidoRepository>();
+builder.Services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+builder.Services.AddScoped<RelatorioVendasService>();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Admin",
+        politica =>
+        {
+            politica.RequireRole("Admin");
+        });
+});
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp));
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddPaging(options =>
+{
+    options.ViewName = "Bootstrap4";
+    options.PageParameterName = "pageindex";
+});
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+CriarPerfisUsuarios(app);
+
+app.UseSession();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+     name: "areas",
+     pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}");
+
+    endpoints.MapControllerRoute(
+       name: "categoriaFiltro",
+       pattern: "Lanche/{action}/{categoria?}",
+       defaults: new { Controller = "Lanche", action = "List" });
+
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
+void CriarPerfisUsuarios(WebApplication app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<ISeedUserRoleInitial>();
+        service.SeedUsers();
+        service.SeedRoles();
+    }
 }
